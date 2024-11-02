7df78de4fb4600ca6301e069ee3a942f57ea60ba
u-kai <76635578+u-kai@users.noreply.github.com>
2024-11-02 16:14:10 +0900

    add search botton

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,24 @@ import NavigationBar from "./components/navigation-bar";
 import ImagesContainer from "./components/home/images-container";
 import TitleBar from "./components/home/title-bar";
 import { toImagePath } from "./common";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <div className="fixed top-10 w-full">
+      <div className="fixed top-10 w-full relative">
         <TitleBar />
+        <div className="absolute right-4 top-4">
+          <Image
+            src={toImagePath("search.svg")}
+            width={30}
+            height={30}
+            alt="logo"
+          />
+        </div>
       </div>
       <div
-        className="relative m-2 top-20 overflow-y-scroll"
+        className="relative mt-4 top-10 overflow-y-scroll"
         style={{ height: "calc(100vh - 10rem)" }}
       >
         <div className="absolute inset-x-0 h-hull flex justify-center">
